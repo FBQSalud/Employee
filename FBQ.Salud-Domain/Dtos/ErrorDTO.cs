ce6cf160b4847c9ba88b9b25cf3ff4c0b0f5c105
MarianoCarrizo <warlockphantasy@gmail.com>
2022-10-19 03:28:49 -0300

    Error DTO response added.

---
+++ FBQ.Salud-Domain/Dtos/ErrorDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace FBQ.Salud_Domain.Dtos
+{
+    public class ErrorDTO
+    {
+
+        public string statuscode { get; set; } = null!;
+        public string message { get; set; } = null!;
+
+
+
+    }
+}
